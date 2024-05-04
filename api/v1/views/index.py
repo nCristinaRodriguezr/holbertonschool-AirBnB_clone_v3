9f1427fcaa4fcd4e156fa90329d9f77483fab573
nataly <ncristina.rodriguezr@gmail.com>
2024-05-03 21:33:37 -0500

    point 4 init of views line 9

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,10 @@
 define a route in the API that can be accessed via the /status URL
 and that returns a JSON indicating that the API status is "OK".
 """
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    return {"status": "OK"}
+    data = {"status": "OK"}
+    return jsonify(data)
