0825400be1f1f586f199300117e1ad88d8bfd401
nataly <ncristina.rodriguezr@gmail.com>
2024-05-03 21:36:33 -0500

    point 4 init of views line 9

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,6 @@ from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
-def get_status():
+def status():
     data = {"status": "OK"}
-    return jsonify(data)
+    return (jsonify(data))
